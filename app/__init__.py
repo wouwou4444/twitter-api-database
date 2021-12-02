ded9ed88f780ded75c2abf4d7f941fa4b8775874
wouwou4444 <valery.buz@outlook.com>
2021-12-02 16:05:17 +0100

    Change to PG db

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from flask_restx import Api
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route('/hello')
-    def hello():
-        return "Goodbye World!"
-
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
+    
     from .apis.tweets import api as tweets
     api = Api()
     api.add_namespace(tweets)
